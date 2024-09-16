8eaed26197ba76933d27a69a089e1630ae1f48a7
kanedu828 <kanedu2000@gamil.com>
2024-09-16 00:33:34 +0000

    add more guides

--- cordia/view/pages/guide_page.py
+++ cordia/view/pages/guide_page.py
@@ -9,9 +9,13 @@ class GuidePage(Page):
         embed = discord.Embed(title=f"Guide", color=discord.Color.dark_orange())
         image_path = "https://kanedu828.github.io/cordia-assets/assets/guide_page.png"
         embed.set_image(url=image_path)
+        guide_text = (
+            "Naviage the different pages to learn more about the game. If you need more help, feel free to reach out on Discord! "
+            "\nView our wiki for further information: https://github.com/kanedu828/Cordia-Wiki"
+        )
         embed.add_field(
             name="Guide Menu",
-            value="Naviage the different pages to learn more about the game. If you need more help, feel free to reach out on Discord!",
+            value=guide_text,
             inline=False,
         )
 
@@ -98,10 +102,57 @@ class GuidePage(Page):
         embed = discord.Embed(title=f"Stats Guide", color=discord.Color.dark_orange())
         image_path = "https://kanedu828.github.io/cordia-assets/assets/guide_page.png"
         embed.set_image(url=image_path)
-        leaderboard_text = "Daily leaderboards reset every 11:55 PM EST. Players who are top 3 in any of the categories will recieve a trophy."
+        leaderboard_text = (
+            "Daily leaderboards reset every 11:55 PM EST. Players who are top 3 in any of the categories will recieve a trophy."
+            "Only rewards gained from manual attacking are counted towards the daily leaderboard."
+        )
         embed.add_field(
             name="About leaderboards",
             value=leaderboard_text,
+            inline=False,
+        )
+        await interaction.response.edit_message(embed=embed, view=self._create_view())
+
+    async def render_upgrade(self, interaction: discord.Interaction):
+        embed = discord.Embed(title=f"Upgrading Gear Guide", color=discord.Color.dark_orange())
+        image_path = "https://kanedu828.github.io/cordia-assets/assets/guide_page.png"
+        embed.set_image(url=image_path)
+        cores_text = (
+            "Stars increase that stats of your gear. For every star, the cost the upgrade your gear increases. "
+            "Every 5 stars, the stats gained from each star also increases. After you reach 10 stars, you will need fragments"
+            " to continue upgrading your gear. Fragments can be found from monsters and bosses."
+        )
+        embed.add_field(
+            name="Stars",
+            value=cores_text,
+            inline=False,
+        )
+
+        cores_text = (
+            "Cores given random bonus stats to your gear. The better the core (basic, quality, supreme, chaos), "
+            "the better that bonus stats are. You can get anywhere from one to three bonus stats when using cores. "
+            "Basic, quality, and supreme cores can be found from monsters and bosses. Chaos cores can be found from powerful monsters, "
+            "and bosses."
+        )
+        embed.add_field(
+            name="Cores",
+            value=cores_text,
+            inline=False,
+        )
+        await interaction.response.edit_message(embed=embed, view=self._create_view())
+
+    async def render_rebirth(self, interaction: discord.Interaction):
+        embed = discord.Embed(title=f"Rebirth Guide", color=discord.Color.dark_orange())
+        image_path = "https://kanedu828.github.io/cordia-assets/assets/guide_page.png"
+        embed.set_image(url=image_path)
+        rebirth_text = (
+            "If you would like to reset your stats, you can rebirth. When you rebirth, your exp is set back to 0. "
+            "However, you will keep all your gear and items. Once you reach level 50, you will gain extra upgrade points for "
+            "rebirthing. You can use rebirthing to become more powerful."
+        )
+        embed.add_field(
+            name="Rebirth",
+            value=rebirth_text,
             inline=False,
         )
         await interaction.response.edit_message(embed=embed, view=self._create_view())
@@ -112,15 +163,25 @@ class GuidePage(Page):
         stats_button = Button(label="Stats", style=discord.ButtonStyle.blurple)
         stats_button.callback = self.stats_button_callback
 
+        rebirth_button = Button(label="Rebirth", style=discord.ButtonStyle.blurple)
+        rebirth_button.callback = self.rebirth_button_callback
+
         leaderboard_button = Button(
             label="Leaderboard", style=discord.ButtonStyle.blurple
         )
         leaderboard_button.callback = self.leaderboard_button_callback
 
+        upgrade_button = Button(
+            label="Upgrading Gear", style=discord.ButtonStyle.blurple
+        )
+        upgrade_button.callback = self.upgrade_button_callback
+
         back_button = Button(label="Back", style=discord.ButtonStyle.grey, row=4)
         back_button.callback = self.back_button_callback
 
         view.add_item(stats_button)
+        view.add_item(rebirth_button)
+        view.add_item(upgrade_button)
         view.add_item(leaderboard_button)
         view.add_item(back_button)
 
@@ -139,3 +200,11 @@ class GuidePage(Page):
     @only_command_invoker()
     async def leaderboard_button_callback(self, interaction: discord.Interaction):
         await self.render_leaderboard(interaction)
+
+    @only_command_invoker()
+    async def rebirth_button_callback(self, interaction: discord.Interaction):
+        await self.render_rebirth(interaction)
+
+    @only_command_invoker()
+    async def upgrade_button_callback(self, interaction: discord.Interaction):
+        await self.render_upgrade(interaction)
